6ca4467a097655ac6e630856e1563d00116bfef8
Fagner Schwalm da Silva <fagnerschwalm@gmail.com>
2017-06-21 23:14:28 -0300

    Create a module cursos.

--- first-project/src/app/app.module.ts
+++ first-project/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {CursosModule} from './cursos/cursos.module';
 
 import { AppComponent } from './app.component';
 import { MyFirstComponent } from './my-first/my-first.component';
@@ -16,7 +17,8 @@ import { MyFirstWithCliComponent } from './my-first-with-cli/my-first-with-cli.c
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
