011cc290d344be35146d76c2e1d57acd9169d114
Fagner Schwalm da Silva <fagnerschwalm@gmail.com>
2017-06-30 00:06:38 -0300

    Directives - ngSwitch

--- directives/src/app/app.module.ts
+++ directives/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DirectiveNgifComponent } from './directive-ngif/directive-ngif.component';
+import { DirectiveNgswitchComponent } from './directive-ngswitch/directive-ngswitch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectiveNgifComponent
+    DirectiveNgifComponent,
+    DirectiveNgswitchComponent
   ],
   imports: [
     BrowserModule
